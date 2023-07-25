38dc2399e772f09954fc9dd22b68f95c3db6c4d1
Luiz Pereira <luizfper@gmail.com>
2023-07-24 22:10:49 -0400

    adds sql files

---
+++ test_case_sql/original_answers.sql
@@ -0,0 +1,30 @@
+INSERT INTO gerund_originaloutgoing (id, content, type, created_at, updated_at, original_incoming_id)
+VALUES
+(1, '"Viva 100: 100 minutos de ligações locais por R$50 por mês.
+ Viva 300: 300 minutos de ligações locais por R$80 por mês.
+ Viva ilimitado: 500 minutos de ligações locais por R$90 por mês.
+ Viva ilimitado plus: 800 minutos de ligações locais por R$95 por mês.
+ Viva ilimitado plus extra: minutos ilimitados de ligações locais por R$120 por mês."', 'answer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1),
+(2, '"Viva 100 mobile: 100 minutos de ligações locais móveis por R$50 por mês.
+ Viva 300 mobile: 300 minutos de ligações locais móveis por R$80 por mês.
+ Viva ilimitado mobile: 500 minutos de ligações locais móveis por R$90 por mês.
+ Viva ilimitado plus mobile: 800 minutos de ligações locais móveis por R$95 por mês.
+ Viva ilimitado plus extra mobile: minutos ilimitados de ligações locais móveis por R$120 por mês."', 'answer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 2),
+(3, 'O Viva ilimitado plus inclui 800 minutos de ligações locais, enquanto o Viva ilimitado plus extra oferece minutos ilimitados de ligações locais.', 'answer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 3),
+(4, 'Não há contrato mínimo para assinar os planos, exceto para o Plano "Eu Aqui e Lá", que requer um contrato de 24 meses.', 'answer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 4),
+(5, 'Sim, você pode escolher planos diferentes para telefonia fixa e móvel. A Viva oferece planos separados para cada tipo de serviço.', 'answer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 5),
+(6, 'A taxa de instalação é de R$1000, mas se você assinar um contrato de 24 meses, receberá um desconto de 90% sobre a instalação. Isso significa que você pagará apenas R$100 pela taxa de instalação. No entanto, se você cancelar antes do prazo, será cobrada a totalidade da instalação, proporcional ao tempo restante do contrato.', 'answer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 6),
+(7, 'Sim, o plano "Eu Aqui e Lá" inclui chamadas internacionais. Você poderá fazer chamadas para qualquer telefone, tanto local quanto internacionalmente.', 'answer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 7),
+(8, 'Além de Belo Horizonte, São Paulo e Rio de Janeiro, a Viva não atua em outras cidades no momento.', 'answer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 8),
+(9, 'Para entrar em contato com o suporte ao cliente da Viva, você pode ligar para o nosso número de atendimento, que funciona 24 horas por dia, sete dias por semana. O tempo de espera é garantido em menos de cinco minutos.', 'answer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 9),
+(10, 'As opções de pagamento disponíveis são débito em conta ou cartão de crédito. Você pode escolher a forma de pagamento que for mais conveniente para você.', 'answer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 10),
+(11, 'Não há limite de dados para os planos de telefonia móvel. No entanto, é importante lembrar que os planos da Viva incluem apenas chamadas de voz. Para internet, você precisará entrar em contato com um provedor de internet da sua cidade.', 'answer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 11),
+(12, 'A Viva oferece apenas serviços de telefonia fixa e móvel. Para internet ou TV a cabo, recomendamos que você entre em contato com os provedores especializados nessas áreas.', 'answer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 12),
+(13, 'Sim, é possível transferir seu número de telefone atual para a Viva. Como cliente novo, você terá a opção de transferir seu número existente ou obter um novo número da Viva.', 'answer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 13),
+(14, 'A Viva não oferece serviços de internet. Recomendamos que você entre em contato com um provedor de internet da sua cidade para obter informações sobre as velocidades disponíveis.', 'answer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 14),
+(15, 'Com exceção do plano "Eu Aqui e Lá", os demais planos cobram tarifas adicionais para chamadas nacionais e internacionais. Essas tarifas variam de acordo com o destino da chamada.', 'answer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 15),
+(16, 'O tempo de instalação do serviço pode variar, mas nos esforçamos para realizar a instalação o mais rápido possível após a contratação. Nosso objetivo é garantir que você tenha acesso aos serviços da Viva o mais breve possível.', 'answer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 16),
+(17, 'Exceto para o novo Plano "Eu Aqui e Lá", os demais planos não têm fidelidade. Você pode cancelar o serviço a qualquer momento sem custos adicionais. No entanto, se você assinar o Plano "Eu Aqui e Lá" com o contrato de 24 meses, será necessário cumprir o prazo mínimo estabelecido. Em caso de cancelamento antes do prazo, será cobrada a totalidade da instalação proporcional ao tempo restante do contrato.', 'answer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 17),
+(18, 'Sim, a Viva possui o melhor suporte técnico do país. Nosso suporte técnico funciona 24 horas por dia, sete dias por semana, e estamos prontos para ajudar em qualquer problema técnico que você possa enfrentar.', 'answer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 18),
+(19, 'Não há limite de minutos para chamadas locais nos planos da Viva, com exceção dos planos que já oferecem minutos ilimitados, como o Viva ilimitado e o Viva ilimitado plus.', 'answer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 19),
+(20, 'O Plano "Eu Aqui e Lá" oferece chamadas ilimitadas para qualquer telefone, incluindo chamadas locais, nacionais e internacionais para telefones fixos e móveis. Além disso, o plano possui um desconto de 100% na taxa de instalação.', 'answer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 20);
---
+++ test_case_sql/original_questions.sql
@@ -0,0 +1,23 @@
+INSERT INTO gerund_originalincoming (id, content, type, created_at, updated_at)
+VALUES
+
+(1, 'Quais são os planos de telefonia fixa disponíveis?', 'question', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(2, 'Quais são os planos de telefonia móvel disponíveis?', 'question', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(3, 'Qual é a diferença entre o plano Viva ilimitado plus e o Viva ilimitado plus extra?', 'question', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(4, 'Há algum contrato mínimo para assinar os planos?', 'question', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(5, 'Posso escolher planos diferentes para telefonia fixa e móvel?', 'question', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(6, 'Como funciona a taxa de instalação e o desconto de 90%?', 'question', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(7, 'O plano "Eu Aqui e Lá" inclui chamadas internacionais?', 'question', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(8, 'Quais cidades a Viva atua além de Belo Horizonte, São Paulo e Rio de Janeiro?', 'question', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(9, 'Como posso entrar em contato com o suporte ao cliente da Viva?', 'question', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(10, 'Quais são as opções de pagamento disponíveis?', 'question', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(11, 'Existe algum limite de dados para os planos de telefonia móvel?', 'question', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(12, 'A Viva oferece serviços adicionais, como internet ou TV a cabo?', 'question', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(13, 'É possível transferir meu número de telefone atual para a Viva?', 'question', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(14, 'Quais são as velocidades de internet oferecidas pela Viva?', 'question', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(15, 'Existe algum custo adicional para chamadas de longa distância?', 'question', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(16, 'Quanto tempo leva para a instalação do serviço?', 'question', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(17, 'Os planos têm fidelidade? Quais as condições para cancelamento?', 'question', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(18, 'A Viva oferece algum tipo de suporte técnico?', 'question', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(19, 'Há algum limite de minutos para chamadas locais?', 'question', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(20, 'Quais os benefícios adicionais do Plano "Eu Aqui e Lá" em relação aos outros planos?', 'question', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
