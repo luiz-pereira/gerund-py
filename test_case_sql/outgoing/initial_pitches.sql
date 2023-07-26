e8bedce4fc5e835443f1737991080f3aa1921230
Luiz Pereira <luizfper@gmail.com>
2023-07-25 21:30:01 -0400

    built sql seeds

---
+++ test_case_sql/outgoing/initial_pitches.sql
@@ -0,0 +1,8 @@
+INSERT INTO gerund_outgoingmessages (content, type, created_at, updated_at)
+VALUES
+-- INITIAL PITCHES
+('Sabe, a Viva está com um novo plano incrível chamado "Eu Aqui e Lá". Com ele, você tem chamadas ilimitadas para qualquer telefone, em qualquer lugar, por apenas R$1000 por mês! Ah, e ainda tem um desconto de 100% na taxa de instalação. É uma oportunidade imperdível!', 'initial_pitch', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Olha só, tenho uma novidade incrível para você! A Viva lançou o Plano "Eu Aqui e Lá", com chamadas ilimitadas para qualquer telefone, em qualquer lugar. E o melhor de tudo, por apenas R$1000 por mês. E sabe o que mais? A taxa de instalação tem um desconto de 100%!', 'initial_pitch', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Ei, tenho uma oferta imperdível para você! A Viva está lançando o Plano "Eu Aqui e Lá", com chamadas ilimitadas para qualquer telefone, em qualquer lugar. Por apenas R$1000 por mês, você pode falar à vontade. E ainda tem mais: a taxa de instalação está com 100% de desconto!', 'initial_pitch', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Quer saber de uma ótima notícia? A Viva lançou o Plano "Eu Aqui e Lá", com chamadas ilimitadas para qualquer telefone, em qualquer lugar. E o preço? Apenas R$1000 por mês! Ah, e não se preocupe com a taxa de instalação, ela está com 100% de desconto!', 'initial_pitch', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Adivinha só o que a Viva tem para você! O Plano "Eu Aqui e Lá", com chamadas ilimitadas para qualquer telefone, em qualquer lugar. A melhor parte? Custa apenas R$1000 por mês! E tem mais uma vantagem: a taxa de instalação tem um desconto de 100%!', 'initial_pitch', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
