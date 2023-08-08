075a121a576bca765ac92bba043f5b20c9a00acf
Luiz Pereira <luiz@fireworkhq.com>
2023-08-07 21:53:27 -0400

    adds greeting sql

---
+++ test_case_sql/outgoing/greetings.sql
@@ -0,0 +1,6 @@
+INSERT INTO gerund_outgoingmessages (content, type, created_at, updated_at)
+VALUES
+-- GREETINGS
+('Oi! Aqui é a Adriana da Viva, com quem tenho o prazer de conversar hoje?', 'greeting', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Oi! Aqui é a Adriana, posso tomar um pouquinho do seu tempo?', 'greeting', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Oi! Aqui é a Viva, posso tomar um pouquinho do seu tempo?', 'greeting', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
