8af2f9f4689a46d209b48e0ab49f22b93e616291
Luiz Pereira <luizfper@gmail.com>
2023-08-22 09:52:04 -0400

    partial work on FE

--- backend/gerund/models.py
+++ backend/gerund/models.py
@@ -15,11 +15,12 @@ class Question(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    script = models.ForeignKey('Script', on_delete=models.CASCADE)
 
     def _str_(self):
         return self.id
 
-class OutgoingMessages(models.Model):
+class OutgoingMessage(models.Model):
     content = models.TextField()
     type = models.CharField(max_length=36)
     answer = models.ForeignKey('Answer', on_delete=models.CASCADE, null=True)
@@ -30,11 +31,21 @@ class OutgoingMessages(models.Model):
     def _str_(self):
         return self.id
 
-class IncomingEmbeddings(models.Model):
+class IncomingEmbedding(models.Model):
     content = models.TextField()
     type = models.CharField(max_length=36)
     question = models.ForeignKey('Question', on_delete=models.CASCADE, null=True)
     embedding = VectorField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def _str_(self):
+        return self.id
+
+class Script(models.Model):
+    custom_prompt = models.TextField()
+    presentation = models.TextField()
+    new_product = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- backend/gerund/views.py
+++ backend/gerund/views.py
@@ -1,22 +1,26 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import OutgoingMessagesSerializer, AnswerSerializer, IncomingEmbeddingsSerializer, QuestionSerializer
-from .models import Answer, Question, OutgoingMessages, IncomingEmbeddings
+from .serializers import OutgoingMessageSerializer, AnswerSerializer, IncomingEmbeddingSerializer, QuestionSerializer, ScriptSerializer
+from .models import Answer, Question, OutgoingMessage, IncomingEmbedding, Script
 
 # Create your views here.
 
 class OutgoingVariationView(viewsets.ModelViewSet):
-    serializer_class = OutgoingMessagesSerializer
-    queryset = OutgoingMessages.objects.all()
+    serializer_class = OutgoingMessageSerializer
+    queryset = OutgoingMessage.objects.all()
 
 class AnswerView(viewsets.ModelViewSet):
     serializer_class = AnswerSerializer
     queryset = Answer.objects.all()
 
 class IncomingVariationView(viewsets.ModelViewSet):
-    serializer_class = IncomingEmbeddingsSerializer
-    queryset = IncomingEmbeddings.objects.all()
+    serializer_class = IncomingEmbeddingSerializer
+    queryset = IncomingEmbedding.objects.all()
 
 class QuestionView(viewsets.ModelViewSet):
     serializer_class = QuestionSerializer
     queryset = Question.objects.all()
+
+class ScriptView(viewsets.ModelViewSet):
+    serializer_class = ScriptSerializer
+    queryset = Script.objects.all()
